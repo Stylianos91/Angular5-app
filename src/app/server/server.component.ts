b720ae6839dcc7161eb12621fff46773ab45c363
Manousakis, Stylianos <Stylianos.Manousakis@ca.com>
2018-03-15 18:25:49 +0100

    Working Databinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true ;
+    }, 2000);
+  }
+
+  serverID: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was Created';
+  }
+
+  onUpdateServerName(event: any) {
+  console.log(event);
+  this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  ngOnInit() {
+  }
 }
