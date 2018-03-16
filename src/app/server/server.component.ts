bfd8d281417a46a7b2650f97f3c807b1fc30aac9
Manousakis, Stylianos <Stylianos.Manousakis@ca.com>
2018-03-16 18:11:43 +0100

    Refactoring

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,23 +4,19 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles: [`.online { color: white}`]
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
-export class ServerComponent implements OnInit {
-  allowNewServer: boolean = false;
-  serverCreationStatus = 'No server was created';
-  serverName = '';
-  serverCreated: boolean = false;
-  serverID: number = 10;
+export class ServerComponent {
+  serverId: number = 10;
   serverStatus: string = 'offline';
 
   constructor() {
-    // setTimeout(() => {
-     this.allowNewServer = true ;
-    // }, 2000);    // if Math.random >0.5 then 'online' else 'offline';
     this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
-
 
   getServerStatus() {
     return this.serverStatus;
@@ -28,18 +24,5 @@ export class ServerComponent implements OnInit {
 
   getColor() {
     return this.serverStatus === 'online' ? 'green' : 'red';
-  }
-
-  onCreateServer() {
-    this.serverCreated = true;
-    this.serverCreationStatus = 'Server was Created with Name ' + this.serverName;
-  }
-
-  onUpdateServerName(event: any) {
-    console.log(event);
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
-
-  ngOnInit() {
   }
 }
