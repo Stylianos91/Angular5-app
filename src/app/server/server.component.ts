b2f8e9535ba77afbf98e502809d69718ab75168f
Manousakis, Stylianos <Stylianos.Manousakis@ca.com>
2018-03-16 15:55:43 +0100

    example of ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`.online { color: white}`]
 })
 export class ServerComponent implements OnInit {
   allowNewServer: boolean = false;
