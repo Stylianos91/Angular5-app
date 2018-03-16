a4103361502c08c0525bca7390e63b1c54f9b8c0
Manousakis, Stylianos <Stylianos.Manousakis@ca.com>
2018-03-16 15:17:37 +0100

    example of ngStyle Usage

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,27 +10,33 @@ export class ServerComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName = '';
   serverCreated: boolean = false;
-
-  constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true ;
-    }, 2000);
-  }
-
   serverID: number = 10;
   serverStatus: string = 'offline';
+
+  constructor() {
+    // setTimeout(() => {
+    // this.allowNewServer = true ;
+    // }, 2000);    // if Math.random >0.5 then 'online' else 'offline';
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
 
   getServerStatus() {
     return this.serverStatus;
   }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
   onCreateServer() {
     this.serverCreated = true;
     this.serverCreationStatus = 'Server was Created with Name ' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
-  console.log(event);
-  this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
   ngOnInit() {
