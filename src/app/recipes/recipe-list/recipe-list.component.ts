62e4e4b907e7a03c866964a84c755dd6a33baf11
Manousakis, Stylianos <Stylianos.Manousakis@ca.com>
2018-03-23 15:36:32 +0100

    Outputting List of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe [] = [new Recipe('Some name for recipe', 'Some description for recipe' ,
+    'http://eatdrinkpaleo.com.au/wp-content/uploads/2016/05/Vietnamese-lemongrass-pork-recipe_900-1.jpg'),
+    new Recipe('Some name for recipe', 'Some description for recipe' ,
+      'http://eatdrinkpaleo.com.au/wp-content/uploads/2016/05/Vietnamese-lemongrass-pork-recipe_900-1.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
